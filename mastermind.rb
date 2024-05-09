ecd4c85cd3e740eeb019b89cc2ed66d5ac81361f
Seeba184 <seenasimkani@gmail.com>
2024-05-09 09:32:16 -0400

    Add computer_code method to generate random code

--- mastermind.rb
+++ mastermind.rb
@@ -13,3 +13,15 @@ class MasterMind
     @first_call_computer_code = false
   end
 end
+
+protected
+
+def computer_code
+  unless @first_call_computer_code
+    @first_call_computer_code = true
+    arr = (1..6).to_a
+    4.times { code << arr.sample.to_s }
+    code
+  end
+  code
+end
